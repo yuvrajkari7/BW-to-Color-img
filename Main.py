c568deda1fb84a71e64b2b01a3a24fb0dc463223
Yuvraj Kari <62334680+yuvrajkari7@users.noreply.github.com>
2023-08-17 00:46:07 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,24 @@
+"""
+Credits: 
+	1. https://github.com/opencv/opencv/blob/master/samples/dnn/colorization.py
+	2. http://richzhang.github.io/colorization/
+	3. https://github.com/richzhang/colorization/
+"""
+
 import numpy as np
 import cv2
 import os
 
+
+
+
+"""
+Download the model files: 
+	1. colorization_deploy_v2.prototxt:    https://github.com/richzhang/colorization/tree/caffe/colorization/models
+	2. pts_in_hull.npy:					   https://github.com/richzhang/colorization/blob/caffe/colorization/resources/pts_in_hull.npy
+	3. colorization_release_v2.caffemodel: https://www.dropbox.com/s/dx0qvhhp5hbcx7z/colorization_release_v2.caffemodel?dl=1
+
+"""
 # load the model and image path
 prototxt_path='model/colorization_deploy_v2.prototxt'
 model_path='model/colorization_release_v2.caffemodel'
